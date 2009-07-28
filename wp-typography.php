e6f08208aa375b3f05f7d06b51dc2dad4585c64c
Jeffrey D. King <jeff.king@weathersource.com>
2009-07-28 18:29:08 +0000

    Changed version number to 1.6
    git-svn-id: http://plugins.svn.wordpress.org/wp-typography/trunk@140164 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-typography.php
+++ wp-typography.php
@@ -5,7 +5,7 @@
 	Description: Improve your web typography with: (1) hyphenation &mdash; over 40 languages supported, (2) Space control, includes: widow protection, gluing values to units, and forced internal wrapping of long URLs & email addresses, (3) Intelligent character replacement, including smart handling of: quote marks, dashes, ellipses, trademarks, math symbols, fractions, and ordinal suffixes, and (4) CSS hooks for styling: ampersands, uppercase words, numbers,  initial quotes &amp; guillemets.
 	Author: Jeffrey D. King
 	Author URI: http://kingdesk.com/
-	Version: 1.5
+	Version: 1.6
 	
 		Copyright 2009, KINGdesk, LLC. Licensed under the GNU General Public License 2.0. If you use, modify and/or redistribute this software, you must leave the KINGdesk, LLC copyright information, the request for a link to http://kingdesk.com, and the web design services contact information unchanged. If you redistribute this software, or any derivative, it must be released under the GNU General Public License 2.0. This program is distributed without warranty (implied or otherwise) of suitability for any particular purpose. See the GNU General Public License for full license terms <http://creativecommons.org/licenses/GPL/2.0/>.
 
