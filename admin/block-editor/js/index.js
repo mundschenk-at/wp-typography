64cf3a9cf1386574fd7dc707899a1896ca62269e
Peter Putzer <github@mundschenk.at>
2020-05-16 12:33:18 +0200

    Split sidebar plugin into its own folder and add proper file header docs

--- admin/block-editor/js/index.js
+++ admin/block-editor/js/index.js
@@ -81,37 +81,202 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./admin/blocks/src/index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./admin/block-editor/src/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./admin/blocks/src/index.js":
-/*!***********************************!*\
-  !*** ./admin/blocks/src/index.js ***!
-  \***********************************/
+/***/ "./admin/block-editor/src/index.js":
+/*!*****************************************!*\
+  !*** ./admin/block-editor/src/index.js ***!
+  \*****************************************/
 /*! no exports provided */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/element */ "@wordpress/element");
-/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _babel_runtime_helpers_defineProperty__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/defineProperty */ "./node_modules/@babel/runtime/helpers/defineProperty.js");
+/* harmony import */ var _babel_runtime_helpers_defineProperty__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_helpers_defineProperty__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @wordpress/plugins */ "@wordpress/plugins");
 /* harmony import */ var _wordpress_plugins__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @wordpress/edit-post */ "@wordpress/edit-post");
-/* harmony import */ var _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @wordpress/components */ "@wordpress/components");
-/* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__);
-/* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @wordpress/data */ "@wordpress/data");
-/* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_wordpress_data__WEBPACK_IMPORTED_MODULE_4__);
-/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @wordpress/i18n */ "@wordpress/i18n");
-/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_wordpress_i18n__WEBPACK_IMPORTED_MODULE_5__);
+!(function webpackMissingModule() { var e = new Error("Cannot find module './blocks/typography'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+/* harmony import */ var _plugins_sidebar_post_toggle__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./plugins/sidebar-post-toggle */ "./admin/block-editor/src/plugins/sidebar-post-toggle/index.js");
+
+/**
+ * This file is part of wp-Typography.
+ *
+ * Copyright 2020 Peter Putzer.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * ***
+ *
+ * @file     This file handles the Gutenberg integration of the wp-Typography plugin.
+ * @author   Peter Putzer <github@mundschenk.at>
+ * @since    5.7.0
+ * @requires Gutenberg 4.3
+ */
+
+/**
+ * WordPress dependencies
+ */
+
+
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _babel_runtime_helpers_defineProperty__WEBPACK_IMPORTED_MODULE_0___default()(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+
+/**
+ * Internal dependencies
+ */
+
+
+ // Register all our blocks.
+
+[!(function webpackMissingModule() { var e = new Error("Cannot find module './blocks/typography'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())].forEach(function (block) {
+  if (!block) {
+    return;
+  }
+
+  var metadata = block.metadata,
+      settings = block.settings,
+      name = block.name;
+  Object(_wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__["registerBlockType"])(name, _objectSpread(_objectSpread({}, metadata), settings));
+}); // Register the plugins as well.
+
+[_plugins_sidebar_post_toggle__WEBPACK_IMPORTED_MODULE_3__].forEach(function (plugin) {
+  if (!plugin) {
+    return;
+  }
+
+  var name = plugin.name,
+      settings = plugin.settings;
+  Object(_wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__["registerPlugin"])(name, settings);
+});
+
+/***/ }),
+
+/***/ "./admin/block-editor/src/plugins/sidebar-post-toggle/index.js":
+/*!*********************************************************************!*\
+  !*** ./admin/block-editor/src/plugins/sidebar-post-toggle/index.js ***!
+  \*********************************************************************/
+/*! exports provided: name, settings */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "name", function() { return name; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "settings", function() { return settings; });
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/element */ "@wordpress/element");
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @wordpress/edit-post */ "@wordpress/edit-post");
+/* harmony import */ var _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_wordpress_edit_post__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _toggle_control__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./toggle-control */ "./admin/block-editor/src/plugins/sidebar-post-toggle/toggle-control.js");
+
+/**
+ * This file is part of wp-Typography.
+ *
+ * Copyright 2020 Peter Putzer.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * ***
+ *
+ * @file    This file handles the Gutenberg sidebar provided by the wp-Typography plugin.
+ * @author  Peter Putzer <github@mundschenk.at>
+ * @since   5.7.0
+ * @requires Gutenberg 4.3
+ */
+
+/**
+ * WordPress dependencies
+ */
+
+
+
+/**
+ * Internal dependencies
+ */
+
+
+/**
+ * Plugin metadata.
+ */
+
+var name = 'wp-typography-sidebar';
+var icon = 'format-quote';
+/**
+ * Renders the plugin.
+ *
+ * @return {PluginDocumentSettingPanel} The sidebar component.
+ */
+
+var render = function render() {
+  return Object(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__["createElement"])(_wordpress_edit_post__WEBPACK_IMPORTED_MODULE_1__["PluginDocumentSettingPanel"], {
+    name: "wp-typography-settings-panel",
+    title: "wp-Typography"
+  }, Object(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__["createElement"])(_toggle_control__WEBPACK_IMPORTED_MODULE_2__["default"], null));
+}; // Export settings.
+
+
+var settings = {
+  icon: icon,
+  render: render
+};
+
+/***/ }),
+
+/***/ "./admin/block-editor/src/plugins/sidebar-post-toggle/toggle-control.js":
+/*!******************************************************************************!*\
+  !*** ./admin/block-editor/src/plugins/sidebar-post-toggle/toggle-control.js ***!
+  \******************************************************************************/
+/*! exports provided: TypographyToggleControl, default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TypographyToggleControl", function() { return TypographyToggleControl; });
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/element */ "@wordpress/element");
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _wordpress_compose__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @wordpress/compose */ "@wordpress/compose");
+/* harmony import */ var _wordpress_compose__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_wordpress_compose__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @wordpress/data */ "@wordpress/data");
+/* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_wordpress_data__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @wordpress/i18n */ "@wordpress/i18n");
+/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @wordpress/components */ "@wordpress/components");
+/* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_wordpress_components__WEBPACK_IMPORTED_MODULE_4__);
 
 /**
  * wp-Typography
  *
- * The sidebar provided by the wp-Typography plugin.
+ * A toggle switch for disabling wp-Typography for specific posts.
  *
  * @requires Gutenberg 4.3
  */
@@ -125,25 +290,21 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-
-
 var TypographyToggleControl = function TypographyToggleControl(props) {
-  return Object(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__["createElement"])(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__["ToggleControl"], {
-    label: Object(_wordpress_i18n__WEBPACK_IMPORTED_MODULE_5__["__"])('Enable enhancements.', 'wp-typography'),
-    help: Object(_wordpress_i18n__WEBPACK_IMPORTED_MODULE_5__["__"])('No help is coming for you.', 'wp-typography'),
+  return Object(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__["createElement"])(_wordpress_components__WEBPACK_IMPORTED_MODULE_4__["ToggleControl"], {
+    label: Object(_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__["__"])('Enable enhancements.', 'wp-typography'),
+    help: Object(_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__["__"])('No help is coming for you.', 'wp-typography'),
     checked: props.typographyEnabled,
     onChange: function onChange(checked) {
       props.setTypographyEnabled(checked);
     }
   });
 };
-
-var TypographyToggleControlWithData = Object(_wordpress_data__WEBPACK_IMPORTED_MODULE_4__["withSelect"])(function (select) {
+/* harmony default export */ __webpack_exports__["default"] = (Object(_wordpress_compose__WEBPACK_IMPORTED_MODULE_1__["compose"])([Object(_wordpress_data__WEBPACK_IMPORTED_MODULE_2__["withSelect"])(function (select) {
   return {
     typographyEnabled: !select('core/editor').getEditedPostAttribute('meta').wp_typography_post_enhancements_disabled
   };
-})(TypographyToggleControl);
-var TypographyToggleControlWithDataAndActions = Object(_wordpress_data__WEBPACK_IMPORTED_MODULE_4__["withDispatch"])(function (dispatch) {
+}), Object(_wordpress_data__WEBPACK_IMPORTED_MODULE_2__["withDispatch"])(function (dispatch) {
   return {
     setTypographyEnabled: function setTypographyEnabled(value) {
       dispatch('core/editor').editPost({
@@ -153,19 +314,33 @@ var TypographyToggleControlWithDataAndActions = Object(_wordpress_data__WEBPACK_
       });
     }
   };
-})(TypographyToggleControlWithData);
+})])(TypographyToggleControl));
 
-var Component = function Component() {
-  return Object(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__["createElement"])(_wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2__["PluginDocumentSettingPanel"], {
-    name: "wp-typography-settings-panel",
-    title: "wp-Typography"
-  }, Object(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__["createElement"])(TypographyToggleControlWithDataAndActions, null));
-};
+/***/ }),
 
-Object(_wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__["registerPlugin"])('wp-typography', {
-  icon: 'format-quote',
-  render: Component
-});
+/***/ "./node_modules/@babel/runtime/helpers/defineProperty.js":
+/*!***************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/defineProperty.js ***!
+  \***************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+module.exports = _defineProperty;
 
 /***/ }),
 
@@ -177,6 +352,17 @@ Object(_wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__["registerPlugin"])('wp-ty
 /***/ (function(module, exports) {
 
 (function() { module.exports = this["wp"]["components"]; }());
+
+/***/ }),
+
+/***/ "@wordpress/compose":
+/*!******************************************!*\
+  !*** external {"this":["wp","compose"]} ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+(function() { module.exports = this["wp"]["compose"]; }());
 
 /***/ }),
 
