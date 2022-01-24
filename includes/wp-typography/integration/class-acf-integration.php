bb093b8fee392e2003ef77d2b4b67082c042d79d
mundschenk_at <github@mundschenk.at>
2022-01-24 22:51:37 +0100

    Meta: Bump copyright year for touched files

--- includes/wp-typography/integration/class-acf-integration.php
+++ includes/wp-typography/integration/class-acf-integration.php
@@ -2,7 +2,7 @@
 /**
  *  This file is part of wp-Typography.
  *
- *  Copyright 2017-2020 Peter Putzer.
+ *  Copyright 2017-2021 Peter Putzer.
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
