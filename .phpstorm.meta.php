24bf36124393f60c59a1e6e47e39a25c3d8f436a
mundschenk_at <github@mundschenk.at>
2022-11-27 02:40:13 +0100

    Meta: Add Mocker support for Intelephense/PHPStorm

---
+++ .phpstorm.meta.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHPSTORM_META;
+
+override(\Mockery::mock(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\Mockery::spy(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\Mockery::namedMock(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\Mockery::instanceMock(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\mock(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\spy(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\namedMock(0), map(["" => "@&\Mockery\MockInterface"]));
