cb89fd216402b4ed20300024c75a8ca3cae1d917
mundschenk_at <github@mundschenk.at>
2023-01-14 23:46:46 +0100

    Docs: Update copyright dates

--- tests/integration/class-acf-integration-test.php
+++ tests/integration/class-acf-integration-test.php
@@ -2,7 +2,7 @@
 /**
  *  This file is part of wp-Typography.
  *
- *  Copyright 2018-2022 Peter Putzer.
+ *  Copyright 2018-2023 Peter Putzer.
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
