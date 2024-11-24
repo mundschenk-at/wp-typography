9ef6bdf00e0b9c8165ca645f212e77a6ca57b452
mundschenk_at <mundschenk-at@users.noreply.github.com>
2024-11-24 18:44:02 +0100

    Rebuilt JS files

--- js/clean-clipboard.js
+++ js/clean-clipboard.js
@@ -1,7 +1,7 @@
 /**
  * This file is part of wp-Typography.
  *
- * Copyright 2016-2021 Peter Putzer.
+ * Copyright 2016-2024 Peter Putzer.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
